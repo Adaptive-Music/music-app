b0a42c3975cd38958ea9334025d8b90bf599fa40
W-Kuah <xiaozhenkuah@hotmail.com>
2024-12-31 07:10:01 +1100

    remove note model redun values

--- lib/component/playScreen/keyboard/key/note_model.dart
+++ lib/component/playScreen/keyboard/key/note_model.dart
@@ -2,18 +2,12 @@ import 'package:music_app/special/enums.dart';
 
 class Note {
   final List<int> keyPitchNotes;
-  final Scale scale;
-  final Octave octave;
   final Instrument instrument;
-  final PlayingMode playingMode;
   
 
   const Note({
     required this.keyPitchNotes,
-    required this.scale,
-    required this.octave,
     required this.instrument,
-    required this.playingMode,
   });
 
 }
